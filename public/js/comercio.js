6b721151547e52b12864725018db6a6f73f4e147
melina frias <meel.frias@gmail.com>
2023-07-06 17:01:38 -0300

    arreglo shop

--- public/js/comercio.js
+++ public/js/comercio.js
@@ -1,13 +1,27 @@
+// cambio titulo segun click filtro
+const dropdownLink = document.getElementById('dropdownLink');
+const dropdownOptions = document.querySelectorAll('.dropdown-menu .dropdown-item');
+
+dropdownOptions.forEach(option => {
+  option.addEventListener('click', function () {
+    const selectedOptionText = this.innerText;
+    dropdownLink.innerHTML = selectedOptionText;
+  });
+});
+
+
+
+// diseño segun click icono
 const grillaButton = document.getElementById('grilla');
 const listaButton = document.getElementById('lista');
 const productosContainer = document.getElementById('productosContainer');
 
-grillaButton.addEventListener('click', function() {
+grillaButton.addEventListener('click', function () {
   productosContainer.classList.remove('lista');
   productosContainer.classList.add('grilla');
 });
 
-listaButton.addEventListener('click', function() {
+listaButton.addEventListener('click', function () {
   productosContainer.classList.remove('grilla');
   productosContainer.classList.add('lista');
 });
